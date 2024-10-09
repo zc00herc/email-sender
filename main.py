b57d1ed160985872fa2be4555ff6da3a9eeecd04
zc00herc <96848546+zc00herc@users.noreply.github.com>
2024-10-09 08:56:22 -0700

    added check for day of week
    checks if today is wednesday and sends an email with a random quote if it is

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import smtplib
 import datetime as dt
+import random
 
+now = dt.datetime.now()
+year = now.year
+month = now.month
+day_of_week = now.weekday()
 my_email = "zack03965@gmail.com"
 password = "aobx neto bpmq pyii"
 
@@ -8,27 +13,22 @@ with open("quotes.txt") as quotes:
     quote_list = quotes.readlines()
     clean_quote_list = [quote.strip() for quote in quote_list]
 
+random_quote = random.choice(clean_quote_list)
 
-with smtplib.SMTP("smtp.gmail.com",port=587) as connection:
-    # Add security encryption
-    connection.starttls()
+if day_of_week == 2:
+    with smtplib.SMTP("smtp.gmail.com",port=587) as connection:
+        # Add security encryption
+        connection.starttls()
 
-    connection.login(user=my_email,password=password)
-    # connection.sendmail(
-    #     from_addr=my_email,
-    #     to_addrs="zack03965@yahoo.com",
-    #     msg="Subject:You are the best\n\n"
-    #         "This is the body of my email"
-    # )
+        connection.login(user=my_email,password=password)
+        connection.sendmail(
+            from_addr=my_email,
+            to_addrs="zack03965@yahoo.com",
+            msg="Subject:Daily Motivation\n\n"
+                f"{random_quote}"
+        )
 
 
-#
-# now = dt.datetime.now()
-# year = now.year
-# month = now.month
-# day_of_week = now.weekday()
-# if year == 2024:
-#     print(day_of_week)
-#
+
 # date_of_birth = dt.datetime(year=2000,month=12,day=25)
 # print(date_of_birth)
